8ec477b65263b3f6d5fdd87bef8ad2fb5e0eb776
Kamuro-tan <>
2022-05-15 17:11:35 +0300

    End 5th lab.

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,6 @@
 
 var main = function() {
-    "use strict";
+    'use strict';
 
     var toDos = [
         "Купить продукты",
@@ -10,39 +10,62 @@ var main = function() {
         "Вывести Грейс на прогулку в парк",
         "Закончить писать книгу"
     ];
+    var new_toDo = "";
 
-    $(".tabs a span").toArray().forEach(function(element) {
-        $(element).on("click", function() {
+    $('.tabs a span').toArray().forEach(function(element) {
+        $(element).on('click', function() {
             var $element = $(element)
             var $content
 
-            $(".tabs a span").removeClass("active");
-            $element.addClass("active");
-            $("main .content").empty();
+            $('.tabs a span').removeClass('active');
+            $element.addClass('active');
+            $('main .content').empty();
 
-            if ($element.parent().is(":nth-child(1)")) {
-                $content = $("<ul>")
-                toDos.forEach(function(todo) {
-                    $content.append($("<li>").text(todo));
-                });
-
-            } else if ($element.parent().is(":nth-child(2)")) {
-                $content = $("<ul>")
+            if ($element.parent().is(':nth-child(1)')) {
+                $content = $('<ul>');
                 for (var i = (toDos.length - 1); i > -1; i--) {
-                    $content.append($("<li>").text(toDos[i]));
+                    $content.append($('<li>').text(toDos[i]));
                 }
 
-            } else if ($element.parent().is(":nth-child(3)")) {
-                console.log("3 TAG")
+            } else if ($element.parent().is(':nth-child(2)')) {
+                $content = $('<ul>')
+                toDos.forEach(function(todo) {
+                    $content.append($('<li>').text(todo));
+                });
+
+            } else if ($element.parent().is(':nth-child(3)')) {
+                $content = $('<form>');
+                $content.append($('<input>').attr({ type: 'text', 
+                                                    placeholder: 'Введите новую задачу..', 
+                                                    value: new_toDo,
+                                                    id: 'new_toDo' }));
+                $content.append($('<button>').addClass('add_toDo'));
+
             }
 
-            $("main .content").append($content);
+            $('main .content').append($content);
             return false;
         });
     });
 
-    $(".tabs a:first-child span").trigger("click");
+// обработчик изменений строки input
+    $('main .content').on('change', '#new_toDo', function() {
+        new_toDo = $('#new_toDo').val();
+
+        return false;
+    });
+
+// обработчик нажатия кнопки добавления
+    $('main .content').on('click', '.add_toDo', function() {
+        if (new_toDo != "") toDos.push(new_toDo);
+
+        return false;
+    });
+    
+
+    $('.tabs a:first-child span').trigger('click');
 
 }
+
 
 $(document).ready(main);
